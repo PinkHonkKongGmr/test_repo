c062ddf09b921e37a46c7657cb5de42770b673c6
Vladimir Nickolaev <exlnted@gmail.com>
2020-06-30 18:45:20 +0300

    vse fignya mishan

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-(function () {
-	let jj = 01;
-	let c = 'letov';
-	let a = 0.0015;
-	let b = '';
-	let bb = 11;
-	let oo = 121010;
-	let a = 0.0015;
-	let b = '';
-	let c = 'letov';
-	let d = 'all up';
-	let e = 0;
-	let f = 122;
-})();
+let a = 0;
+
+const what = () => a ** 2;
