a6bfd84b60deba84ce2119ce0816447be469c4bf
Vladimir Nickolaev <exlnted@gmail.com>
2020-06-30 14:32:31 +0300

    restructure

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-(function () {
-	let jj = 01;
-	let c = 'letov';
-	let a = 0.0015;
-	let b = '';
-	let bb = 11;
-	let oo = 121010;
-})();
+let createin4 = 100;
+let creatrin42 = 200;
+
+const sum = () => createin4 + creatrin42;
+
+console.log(sum);
