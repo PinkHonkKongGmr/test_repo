2da2398ecf610d41ad3e61c9d1019c2d98ab8bfe
Vladimir Nickolaev <exlnted@gmail.com>
2020-06-30 14:03:39 +0300

    t4c1

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-let createin4 = 100;
-let creatrin42 = 200;
-
-const sum = () => createin4 + creatrin42;
-
-console.log(sum);
+(function () {
+	let jj = 01;
+	let c = 'letov';
+	let a = 0.0015;
+	let b = '';
+	let bb = 11;
+	let oo = 121010;
+	let a = 0.0015;
+	let b = '';
+	let c = 'letov';
+	let d = 'all up';
+	let e = 0;
+	let f = 122;
+})();
